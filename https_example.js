b7e512a25ebd374b63ad92d5e90c0bdc11b481af
Raymond Kwan <rayk199492@gmail.com>
2018-09-02 03:16:45 +0000

    Added codes to retrieve stream from server

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 const https = require('https');
 
-console.log('i did it!');
+const options = {
+  host: 'www.example.org',
+  path: '/',
+};
+
+const callBack = (response) => {
+  console.log('Response', response);
+  console.log('In callBack');
+}
+
+console.log('before request');
+
+https.request(options, callBack).end();
+
+console.log('After request');
+
